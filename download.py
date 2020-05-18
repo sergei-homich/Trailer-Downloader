acd2daf44781463cdf2baf12a8995b83ba5c0b3f
David Engel <david@airshipdevelopment.com>
2020-05-18 16:11:02 -0500

    1.13
    1. YouTube search wasn't iterating through results properly
    
    2. Radarr file environment variable needed a closing slash to work properly; tested on Linux with forward slash and hopefully works with backslashes on Windows as well
    
    3. path was being checked with os.path.isfile() when it needs os.path.isdir()

--- download.py
+++ download.py
@@ -52,7 +52,7 @@ except:
 # Arguments
 def getArguments():
     name = 'Trailer-Downloader'
-    version = '1.12'
+    version = '1.13'
     parser = ArgumentParser(description='{}: download a movie trailer from Apple or YouTube with help from TMDB'.format(name))
     parser.add_argument('-v', '--version', action='version', version='{} {}'.format(name, version), help='show the version number and exit')
     parser.add_argument('-d', '--directory', dest='directory', help='full path of directory to copy downloaded trailer', metavar='DIRECTORY')
